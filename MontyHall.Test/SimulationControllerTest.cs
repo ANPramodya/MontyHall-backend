7d666c8487be261535efa8ff401062a47496c312
ANPramodya <86116471+ANPramodya@users.noreply.github.com>
2023-10-05 14:27:36 +0530

    test project

---
+++ MontyHall.Test/SimulationControllerTest.cs
@@ -0,0 +1,21 @@
+namespace MontyHall.Test
+{
+    public class SimulationControllerTest
+    {
+        [Fact]
+        public void GenerateSimulation_Accepts_SwitchDoor_NoOfGames()
+        {
+            //Arrange
+            //Act
+            //Asert
+        }
+
+        [Fact]
+        public void GenerateSimulation_Returns_ListOfGames() 
+        {
+            //Assert
+            //Act
+            //Asert
+        }
+    }
+}
---
+++ MontyHall.Test/SimulationServiceTest.cs
@@ -0,0 +1,30 @@
+﻿namespace MontyHall.Test
+{
+    public class SimulationServiceTest
+    {
+        [Fact]
+        public void GenerateSimulationService_Returns_ListOf_Games()
+        {
+            //Arrange
+            //Act
+            //Assert
+
+        }
+
+        [Fact]
+        public void HostChoice_Returns_Door_No_Of_NotPrizeDoor()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [Fact]
+        public void PlayerWonOrNotService_Returns_Result()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+    }
+}
